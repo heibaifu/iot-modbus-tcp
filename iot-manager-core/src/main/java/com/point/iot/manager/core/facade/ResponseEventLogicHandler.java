4313848352c508403f9d8f8501163ee7a1b1d2c6
snowwolf <yegu-21@163.com>
2017-05-21 22:03:05 +0800

    delete error code

--- iot-manager-core/src/main/java/com/point/iot/manager/core/facade/ResponseEventLogicHandler.java
+++
@@ -1,7 +0,0 @@
-package com.point.iot.manager.core.facade;
-
-import com.tongqu.game.manager.request.bean.ResponseEvent;
-
-public interface ResponseEventLogicHandler<REQ extends ResponseEvent> {
-	void doExec(REQ req);
-}
